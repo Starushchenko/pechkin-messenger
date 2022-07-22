aa3180baec49513e319b6f1324627710b5ad229e
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2022-07-22 16:06:50 +0300

    Добавил редирект на 404 в express, если страница не найдена

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const PORT = 3000;
 const distFolder = (__dirname + '/../dist/');
 
 app.use(express.static(distFolder));
+
+app.get('*', (req, res) => {
+  res.status(404).type('.html').sendFile('error-404.html', {root: distFolder});
+});
+
 app.listen(PORT, function() {
   console.log(`App is running on port :${PORT}`);
 });
