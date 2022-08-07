dbffce6b6ac6d5dcedc450886d25f0b8fe485869
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2022-08-07 12:43:31 +0300

    Доработал раздачу статики на npm run start

--- server/index.js
+++ server/index.js
@@ -2,11 +2,32 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const distFolder = (__dirname + '/../dist/');
+const PAGES = [
+  'auth',
+  'register',
+  'chats',
+  'chat',
+  'profile',
+  'error-404',
+  'error-500',
+  'change-password',
+  'change-settings'
+]
 
 app.use(express.static(distFolder));
 
+app.get(`/`, (req, res) => {
+  res.type('.html').sendFile(`index/index.html`, {root: distFolder});
+});
+
+PAGES.forEach((item) => {
+  app.get(`**/${item}*`, (req, res) => {
+    res.type('.html').sendFile(`${item}/index.html`, {root: distFolder});
+  });
+})
+
 app.get('**/*', (req, res) => {
-  res.status(404).type('.html').sendFile('error-404.html', {root: distFolder});
+  res.status(404).type('.html').sendFile('error-404/error-404.html', {root: distFolder});
 });
 
 app.listen(PORT, function() {
