d74556986a99bf0c8a60e83a74e826ecd01529df
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2022-08-07 22:25:26 +0300

    Закончены страницы профиля и настроек профиля

---
+++ src/blocks/form/profile-form/form.ts
@@ -0,0 +1,113 @@
+import Block from '../../../utils/Block';
+import IForm from "../interface";
+import {EMAIL, LOGIN, NAME, PHONE} from '../../../enums/validation-rules';
+
+import FormField from '../../../components/form-field/form-field';
+import Button from '../../../components/button/button';
+
+import template from './form.tpl.hbs';
+
+class ProfileForm extends Block {
+  constructor(props: IForm) {
+    super(props);
+  }
+
+  protected initChildren() {
+    this.children['email-field'] = new FormField({
+      name: 'email',
+      label: 'Электронная почта',
+      errorText: EMAIL.errorText,
+      field: {
+        type: 'email',
+        name: 'email',
+        placeholder: 'Например, ivanoff@yandex.ru',
+        required: true,
+        value: 'pochta@yandex.ru',
+        rule: EMAIL.rule
+      }
+    });
+    
+    this.children['login-field'] = new FormField({
+      name: 'login',
+      label: 'Логин',
+      errorText: LOGIN.errorText,
+      field: {
+        type: 'text',
+        name: 'login',
+        placeholder: 'Например, ivanoff123',
+        minlength: 3,
+        maxlength: 20,
+        required: true,
+        value: 'konstantin_ivanov',
+        rule: LOGIN.rule
+      }
+    });
+
+    this.children['name-field'] = new FormField({
+      name: 'name',
+      label: 'Имя',
+      errorText: NAME.errorText,
+      field: {
+        type: 'text',
+        name: 'name',
+        placeholder: 'Например, Константин',
+        required: true,
+        value: 'Константин',
+        rule: NAME.rule
+      }
+    });
+
+    this.children['second-name-field'] = new FormField({
+      name: 'second_name',
+      label: 'Фамилия',
+      errorText: NAME.errorText,
+      field: {
+        type: 'text',
+        name: 'second_name',
+        placeholder: 'Например, Иванов',
+        required: true,
+        value: 'Иванов',
+        rule: NAME.rule
+      }
+    });
+
+    this.children['nickname-field'] = new FormField({
+      name: 'nickname',
+      label: 'Имя в чате',
+      errorText: LOGIN.errorText,
+      field: {
+        type: 'text',
+        name: 'nickname',
+        placeholder: 'Например, mr_ivanoff',
+        required: false,
+        value: 'Mr_ivanoff',
+        rule: LOGIN.rule
+      }
+    });
+
+    this.children['phone-field'] = new FormField({
+      name: 'phone',
+      label: 'Телефон',
+      errorText: PHONE.errorText,
+      field: {
+        type: 'text',
+        name: 'phone',
+        placeholder: '+7 911 911 91 91',
+        required: true,
+        value: '+79099673030',
+        rule: PHONE.rule
+      }
+    });
+
+    this.children['button-save'] = new Button({
+      text: 'Сохранить',
+      type: 'submit'
+    });
+  }
+
+  render() {
+    return this.compile(template, { ...this.props });
+  }
+}
+
+export default ProfileForm;
---
+++ src/pages/change-settings/change-settings.ts
@@ -0,0 +1,57 @@
+import Block from '../../utils/Block';
+import renderDOM from '../../utils/renderDOM';
+
+import Button from '../../components/button/button';
+import ProfileForm from '../../blocks/form/profile-form/form';
+
+import template from './change-settings.tpl.hbs';
+import {formatFormData} from '../../utils/helpers';
+
+class ChangeSettings extends Block {
+  protected initChildren() {
+
+    this.children['button-back'] = new Button({
+      classes: 'button--icon app__back-btn',
+      text: 'Назад',
+      icon: `
+        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect x="17" y="11.8" width="11" height="1.6" transform="rotate(-180 17 11.8)" fill="white"/>
+          <path d="M10 16L6 11L10 6" stroke="white" stroke-width="1.6"/>
+        </svg>
+      `,
+      events: {
+        click: (e) => this.onStepBack(e),
+      }
+    });
+
+    this.children['profile-form'] = new ProfileForm({
+      title: 'Константин',
+      events: {
+        submit: (e) => this.onSubmit(e),
+      },
+    });
+  }
+
+  onSubmit(e: Event) {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    console.log(formatFormData(formData));
+  }
+
+  onStepBack(e: Event) {
+    e.preventDefault();
+    history.back();
+  }
+
+
+  render() {
+    return this.compile(template, {});
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const page = new ChangeSettings();
+
+  renderDOM('#app', page);
+});
