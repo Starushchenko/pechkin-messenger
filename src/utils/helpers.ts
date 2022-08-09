2b64644cffdc4fcf76b54455eead741628e7687b
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2022-08-09 11:45:24 +0300

    Добавил класс HTTPTransport из тренажёра

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -20,3 +20,13 @@ export const formatFormData = (data: FormData) => {
   }
   return result;
 };
+
+export const queryStringify = (data: Record<string, string>): string => {
+  if (typeof data !== 'object') {
+    throw new Error('Not object');
+  }
+  const keys = Object.keys(data);
+  return keys.reduce((result, key, index) => {
+    return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`;
+  }, '?');
+};
